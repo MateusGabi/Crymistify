04ae1a561dea581e621a369e0ef4fd93d2fd88a9
Mateus Gabi <mateusgabimoreira@hotmail.com>
2017-11-16 09:28:56 -0300

    Heroku App files

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const forceSSL = function () {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+
+// app.use(forceSSL());
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/build'));
+
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
