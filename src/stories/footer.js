3e0504857f1958a9c660906c2fd40a7a40549b9b
Mateus Gabi <mateusgabimoreira@gmail.com>
2018-03-31 15:38:22 +0000

    test(Test): corrigido bugs do ESLint

--- src/stories/footer.js
+++ src/stories/footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import Footer from '../Components/Footer';
 
 storiesOf('Footer', module).add('default', () => <Footer />);
